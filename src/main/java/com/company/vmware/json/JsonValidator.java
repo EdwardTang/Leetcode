6f20624e456b50d3ae01e91204b580d77614f187
Yongbing Tang <yongbing@lumity.com>
2020-02-15 11:28:21 -0800

    code clean up

--- src/main/java/com/company/vmware/json/JsonValidator.java
+++ src/main/java/com/company/vmware/json/JsonValidator.java
@@ -9,9 +9,9 @@ import java.util.Deque;
 
 public class JsonValidator {
 
-  public  static void main(String[] args) {
+  public static void main(String[] args) {
     String test1 = "{}"; // true
-    String test2 = "{";  //false
+    String test2 = "{"; // false
     String test3 = "{\"x\" : { \"y\" : \"z\"}}"; // true
     String test4 =
         "{ \n"
@@ -41,9 +41,9 @@ public class JsonValidator {
             + "}";
     String test5 = "[{\n" + "\t\"\": \"\"\n" + "}]";
     String test6 = "[\n" + "\t[]";
-//    assertTrue(isValid(test1));
-//    assertFalse(isValid(test2));
-//    assertTrue(isValid(test3));
+    //    assertTrue(isValid(test1));
+    //    assertFalse(isValid(test2));
+    //    assertTrue(isValid(test3));
     assertTrue(isValid(test4));
     assertTrue(isValid(test5));
     assertFalse(isValid(test6));
@@ -52,91 +52,89 @@ public class JsonValidator {
   public static boolean isValid(String s) {
     Deque<Character> stack = new ArrayDeque<>();
     char[] chars = s.toCharArray();
-    for(int i = 0; i < chars.length; i++) {
-//      if(!stack.isEmpty() && stack.peekLast() == ':') {
-//        if(chars[i] == '"' || chars[i] == '{' || chars[i] == '[' || Character.isDigit(chars[i])) {
-//          if(chars[i] == '"' || chars[i] == '{' || chars[i] == '['){
-//            stack.addLast(chars[i]);
-//          } else if (Character.isDigit(chars[i])) {
-//            if(chars[i] == '0'){
-//              stack.addLast(chars[i]);
-//            }
-//          }
-//        } else {
-//          // {"x" : a }
-//          System.out.println("processed: " + s.substring(0, i));
-//          System.out.println("chars[i]: " + chars[i]);
-//          return false;
-//        }
-//      } else if(!stack.isEmpty() && stack.peekLast() == '0') {
-//        if(chars[i] != '.') {
-//          System.out.println("processed: " + s.substring(0, i));
-//          System.out.println("chars[i]: " + chars[i]);
-//          return false;
-//        } else {
-//          stack.addLast(chars[i]);
-//        }
-//      } else if(!stack.isEmpty() && stack.peekLast() == '.') {
-//        if(!Character.isDigit(chars[i])) {
-//          return false;
-//        }
-//      } else if(isFocusSymbol(chars[i])) {
-      if(isFocusSymbol(chars[i])) {
-       if(stack.isEmpty()) {
-         stack.addLast(chars[i]);
-       } else {
-         // stack is not empty
-         char top = stack.peekLast();
-         if(!isValidTop(top)) {
-           System.out.println("processed: " + s.substring(0, i));
-           System.out.println("chars[i]: " + chars[i]);
-           return false;
-         } else {
-           // top can be , { [ " :
-           if(top == '{') {
-             if(chars[i] ==']') {
-               System.out.println("processed: " + s.substring(0, i));
-               System.out.println("chars[i]: " + chars[i]);
-               return false;
-             } else if (chars[i] == '}'){
-               stack.removeLast();
-             } else {
-               stack.addLast(chars[i]);
-
-             }
-           } else if (top == '[') {
-             if( chars[i] == ':' || chars[i] == '}') {
-               System.out.println("processed: " + s.substring(0, i));
-               System.out.println("chars[i]: " + chars[i]);
-               return false;
-             } else if (chars[i] == ']') {
-               stack.removeLast();
-             } else {
-               stack.addLast(chars[i]);
-             }
-           } else if (top == ':' || top == ',') {
-             if(chars[i] == ']' || chars[i] == '}' || chars[i] == ',' || chars[i] == ':') {
-               System.out.println("processed: " + s.substring(0, i));
-               System.out.println("chars[i]: " + chars[i]);
-               return false;
-             } else {
-               //  if (chars[i] == '{' || chars[i] == '[' || chars[i] == '"')
-               stack.removeLast();
-               stack.addLast(chars[i]);
-             }
-           } else  {
-             // if (top == '"')
-             if(chars[i] == '"') {
-               stack.removeLast();
-             } else {
-               // skip any character inside the quotation mark
-               continue;
-             }
-           }
-         }
-
-
-       }
+    for (int i = 0; i < chars.length; i++) {
+      //      if(!stack.isEmpty() && stack.peekLast() == ':') {
+      //        if(chars[i] == '"' || chars[i] == '{' || chars[i] == '[' ||
+      // Character.isDigit(chars[i])) {
+      //          if(chars[i] == '"' || chars[i] == '{' || chars[i] == '['){
+      //            stack.addLast(chars[i]);
+      //          } else if (Character.isDigit(chars[i])) {
+      //            if(chars[i] == '0'){
+      //              stack.addLast(chars[i]);
+      //            }
+      //          }
+      //        } else {
+      //          // {"x" : a }
+      //          System.out.println("processed: " + s.substring(0, i));
+      //          System.out.println("chars[i]: " + chars[i]);
+      //          return false;
+      //        }
+      //      } else if(!stack.isEmpty() && stack.peekLast() == '0') {
+      //        if(chars[i] != '.') {
+      //          System.out.println("processed: " + s.substring(0, i));
+      //          System.out.println("chars[i]: " + chars[i]);
+      //          return false;
+      //        } else {
+      //          stack.addLast(chars[i]);
+      //        }
+      //      } else if(!stack.isEmpty() && stack.peekLast() == '.') {
+      //        if(!Character.isDigit(chars[i])) {
+      //          return false;
+      //        }
+      //      } else if(isFocusSymbol(chars[i])) {
+      if (isFocusSymbol(chars[i])) {
+        if (stack.isEmpty()) {
+          stack.addLast(chars[i]);
+        } else {
+          // stack is not empty
+          char top = stack.peekLast();
+          if (!isValidTop(top)) {
+            System.out.println("processed: " + s.substring(0, i));
+            System.out.println("chars[i]: " + chars[i]);
+            return false;
+          } else {
+            // top can be , { [ " :
+            if (top == '{') {
+              if (chars[i] == ']') {
+                System.out.println("processed: " + s.substring(0, i));
+                System.out.println("chars[i]: " + chars[i]);
+                return false;
+              } else if (chars[i] == '}') {
+                stack.removeLast();
+              } else {
+                stack.addLast(chars[i]);
+              }
+            } else if (top == '[') {
+              if (chars[i] == ':' || chars[i] == '}') {
+                System.out.println("processed: " + s.substring(0, i));
+                System.out.println("chars[i]: " + chars[i]);
+                return false;
+              } else if (chars[i] == ']') {
+                stack.removeLast();
+              } else {
+                stack.addLast(chars[i]);
+              }
+            } else if (top == ':' || top == ',') {
+              if (chars[i] == ']' || chars[i] == '}' || chars[i] == ',' || chars[i] == ':') {
+                System.out.println("processed: " + s.substring(0, i));
+                System.out.println("chars[i]: " + chars[i]);
+                return false;
+              } else {
+                //  if (chars[i] == '{' || chars[i] == '[' || chars[i] == '"')
+                stack.removeLast();
+                stack.addLast(chars[i]);
+              }
+            } else {
+              // if (top == '"')
+              if (chars[i] == '"') {
+                stack.removeLast();
+              } else {
+                // skip any character inside the quotation mark
+                continue;
+              }
+            }
+          }
+        }
       } else {
         // skip literals
         continue;
@@ -146,7 +144,8 @@ public class JsonValidator {
   }
 
   private static boolean isFocusSymbol(char ch) {
-    return (ch == '{' || ch == '}' || ch == '[' || ch == ']' || ch == ':' || ch == ',' || ch == '"');
+    return (ch == '{' || ch == '}' || ch == '[' || ch == ']' || ch == ':' || ch == ','
+        || ch == '"');
   }
 
   private static boolean isValidTop(char ch) {
--- src/main/java/com/company/vmware/myrand/MyRand.java
+++ src/main/java/com/company/vmware/myrand/MyRand.java
@@ -5,21 +5,23 @@ import java.util.StringJoiner;
 public final class MyRand {
 
   /**
-   * Generate random decimal number between [5,55] inclusively with given rand_0() that randomly return 0 or 1;
+   * Generate random decimal number between [5,55] inclusively with given rand_0() that randomly
+   * return 0 or 1;
+   *
    * @return
    */
   public static int getRand() {
 
     StringJoiner s = new StringJoiner("");
-    while(true) {
-      if(s.length() < 8) {
+    while (true) {
+      if (s.length() < 8) {
         s.add(String.valueOf(oneOrZero()));
         continue;
       }
 
       int v = Integer.parseInt(s.toString(), 2);
 
-      if(v >= 5 && v<=55) {
+      if (v >= 5 && v <= 55) {
         return v;
       } else {
         s = new StringJoiner("");
@@ -27,20 +29,18 @@ public final class MyRand {
     }
   }
 
-
   /**
    * rand_0() that randomly return 0 or 1
+   *
    * @return
    */
   private static int oneOrZero() {
-      return (int)Math.round(Math.random());
+    return (int) Math.round(Math.random());
   }
 
   public static void main(String[] args) {
-    for(int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
       System.out.println(MyRand.getRand());
     }
   }
-
-
 }
